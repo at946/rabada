cbcb43c6ccba792f7f55e29d65e0a4f27bcc4862
Asato TAKAHASHI <asato730@gmail.com>
2020-08-09 23:56:57 +0900

    set ogp

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description },
       { hid: 'og:site_name', property: 'og:site_name', content: process.env.npm_package_name || ''},
+      { hid: 'og:title', property: 'og:title', process.env.npm_package_name || '' },
       { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:url', property: 'og:url', content: 'https://rabada.herokuapp.com' },
       { hid: 'og:description', property: 'og:description', content: process.env.npm_package_description },
